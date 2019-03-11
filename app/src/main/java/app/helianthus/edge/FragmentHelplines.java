6efeb9babc45437012d3a6733c798faa347abbec
CheesyBeer <44321294+cnmberches@users.noreply.github.com>
2019-03-11 22:28:02 +0800

    Merge branch 'master' of C:\Users\charles\Edge_app with conflicts.

--- app/src/main/java/app/helianthus/edge/FragmentHelplines.java
+++ app/src/main/java/app/helianthus/edge/FragmentHelplines.java
@@ -1,10 +1,7 @@
 package app.helianthus.edge;
 
-import androidx.constraintlayout.widget.ConstraintLayout;
 import androidx.lifecycle.ViewModelProviders;
 
-import android.content.Intent;
-import android.net.Uri;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
@@ -15,11 +12,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
-import com.google.android.material.button.MaterialButton;
-
-public class FragmentHelplines extends Fragment implements View.OnClickListener{
-    private MaterialButton btn_call, btn_message;
-    private ConstraintLayout btn_link1, btn_link2;
+public class FragmentHelplines extends Fragment {
 
     private FragmentHelplinesViewModel mViewModel;
 
@@ -30,14 +23,7 @@ public class FragmentHelplines extends Fragment implements View.OnClickListener{
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
                              @Nullable Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.fragment_helplines, container, false);
-
-        btn_call = view.findViewById(R.id.helplines_btn_call);
-        btn_message = view.findViewById(R.id.helplines_btn_message);
-        btn_link1 = view.findViewById(R.id.helplines_btn_link1);
-        btn_link2 = view.findViewById(R.id.helplines_btn_link2);
-
-        return view;
+        return inflater.inflate(R.layout.fragment_helplines, container, false);
     }
 
     @Override
@@ -47,30 +33,6 @@ public class FragmentHelplines extends Fragment implements View.OnClickListener{
         // TODO: Use the ViewModel
     }
 
-    @Override
-    public void onClick(View v) {
-        Intent newActivity;
-        switch (v.getId())
-        {
-            case R.id.helplines_btn_call:
-                newActivity = new Intent(Intent.ACTION_CALL);
-                newActivity.setData(Uri.parse("tel:09175584673"));
-                startActivity(newActivity);
-                break;
-            case R.id.helplines_btn_message:
-                newActivity = new Intent(Intent.ACTION_SENDTO, Uri.parse("smsto:09175584673"));
-                startActivity(newActivity);
-                break;
-            case R.id.helplines_btn_link1:
-                newActivity= new Intent(Intent.ACTION_VIEW);
-                newActivity.setData(Uri.parse("https://www.pap.org.ph/"));
-                startActivity(newActivity);
-                break;
-            case R.id.helplines_btn_link2:
-                newActivity= new Intent(Intent.ACTION_VIEW);
-                newActivity.setData(Uri.parse("https://ppa.philpsych.ph/"));
-                startActivity(newActivity);
-                break;
-        }
-    }
+    //Try uli
+    //fasdfsdf
 }
