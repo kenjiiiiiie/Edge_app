13f24eb0f4b51a235ef1f48f37e210761593b165
CheesyBeer <44321294+cnmberches@users.noreply.github.com>
2019-03-11 21:03:00 +0800

    Helplines Complete ugh

--- app/src/main/java/app/helianthus/edge/FragmentJournal.java
+++ app/src/main/java/app/helianthus/edge/FragmentJournal.java
@@ -2,12 +2,16 @@ package app.helianthus.edge;
 
 import androidx.lifecycle.ViewModelProviders;
 
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import android.provider.BaseColumns;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -16,6 +20,13 @@ import android.view.ViewGroup;
 import android.widget.Toast;
 
 public class FragmentJournal extends Fragment {
+    private static final String SQL_CREATE_ENTRIES = "CREATE TABLE " + JournalEntry.TABLE_NAME + " (" +
+            JournalEntry._ID + " INTEGER PRIMARY KEY," +
+            JournalEntry.COLUMN_DATE_TIME + " TEXT," +
+            JournalEntry.COLUMN_ENTRY + " TEXT)";
+
+    private static final String SQL_DELETE_ENTRIES =
+            "DROP TABLE IF EXISTS " + JournalEntry.TABLE_NAME;
 
     private FragmentJournalViewModel mViewModel;
 
@@ -65,4 +76,30 @@ public class FragmentJournal extends Fragment {
         }
     }
 
+    public static class JournalEntry implements BaseColumns {
+        static final String TABLE_NAME = "journal_entry";
+        static final String COLUMN_DATE_TIME = "date_time_entry";
+        static final String COLUMN_ENTRY = "entry";
+    }
+
+    public class JournalDBEntryHelper extends SQLiteOpenHelper {
+        // If you change the database schema, you must increment the database version.
+        public static final int DATABASE_VERSION = 1;
+        public static final String DATABASE_NAME = "FeedReader.db";
+
+        public JournalDBEntryHelper(Context context) {
+            super(context, DATABASE_NAME, null, DATABASE_VERSION);
+        }
+        public void onCreate(SQLiteDatabase db) {
+            db.execSQL(SQL_CREATE_ENTRIES);
+        }
+        public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+
+            db.execSQL(SQL_DELETE_ENTRIES);
+            onCreate(db);
+        }
+        public void onDowngrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+            onUpgrade(db, oldVersion, newVersion);
+        }
+    }
 }
