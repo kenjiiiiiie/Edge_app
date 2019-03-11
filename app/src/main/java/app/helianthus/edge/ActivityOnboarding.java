f4f54655d4cd5a51561954c422c3575d4541e8d1
Kenneth Nuñezca <manilaboy07@gmail.com>
2019-03-11 21:23:16 +0800

    Onboarding

--- app/src/main/java/app/helianthus/edge/ActivityOnboarding.java
+++ app/src/main/java/app/helianthus/edge/ActivityOnboarding.java
@@ -1,146 +1,137 @@
 package app.helianthus.edge;
 
-
-import android.app.Activity;
-
-import androidx.fragment.app.Fragment;
-import androidx.fragment.app.FragmentManager;
-import androidx.fragment.app.FragmentPagerAdapter;
-import androidx.viewpager.widget.ViewPager;
-
 import android.os.Bundle;
-import android.view.LayoutInflater;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
 
-import android.widget.TextView;
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
+import androidx.viewpager.widget.ViewPager;
 
-public class ActivityOnboarding extends Activity {
+public class On_Boarding extends AppCompatActivity implements View.OnClickListener{
 
-    /**
-     * The {@link androidx.viewpager.widget.PagerAdapter} that will provide
-     * fragments for each of the sections. We use a
-     * {@link FragmentPagerAdapter} derivative, which will keep every
-     * loaded fragment in memory. If this becomes too memory intensive, it
-     * may be best to switch to a
-     * androidx.fragment.app.FragmentStatePagerAdapter.
-     */
-    private SectionsPagerAdapter mSectionsPagerAdapter;
-
-    /**
-     * The {@link ViewPager} that will host the section contents.
-     */
     private ViewPager mViewPager;
+    private int[] layouts = {R.layout.fragment_onboarding_01, R.layout.fragment_onboarding_02, R.layout.fragment_onboarding_03, R.layout.fragment_onboarding_04};
+    private PageAdapter pageAdapter;
+
+    private LinearLayout dots_layout;
+    private ImageView[] dots;
+
+    private Button btnSkip, btnFinish, btnNext;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_onboarding);
-        // Create the adapter that will return a fragment for each of the three
-        // primary sections of the activity.
-        mSectionsPagerAdapter = new SectionsPagerAdapter(getFragmentManager());
 
-        // Set up the ViewPager with the sections adapter.
-        mViewPager = (ViewPager) findViewById(R.id.container);
-        mViewPager.setAdapter(mSectionsPagerAdapter);
+        mViewPager = findViewById(R.id.viewPager);
+        pageAdapter = new PageAdapter(layouts, this);
+        mViewPager.setAdapter(pageAdapter);
 
+        dots_layout = findViewById(R.id .dotsLayout);
+        createDots(0);
 
-    }
+        mViewPager.addOnPageChangeListener(new ViewPager.OnPageChangeListener() {
+            @Override
+            public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
 
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_activity_onboarding, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
-
-    /**
-     * A placeholder fragment containing a simple view.
-     */
-    public static class PlaceholderFragment extends Fragment {
-        /**
-         * The fragment argument representing the section number for this
-         * fragment.
-         */
-        private static final String ARG_SECTION_NUMBER = "section_number";
-
-        public PlaceholderFragment() {
-        }
-
-        /**
-         * Returns a new instance of this fragment for the given section
-         * number.
-         */
-        public static PlaceholderFragment newInstance(int sectionNumber) {
-            PlaceholderFragment fragment = new PlaceholderFragment();
-            Bundle args = new Bundle();
-            args.putInt(ARG_SECTION_NUMBER, sectionNumber);
-            fragment.setArguments(args);
-            return fragment;
-        }
-
-        @Override
-        public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                                 Bundle savedInstanceState) {
-            View rootView = inflater.inflate(R.layout.fragment_onboarding_01, container, false);
-            TextView textView = (TextView) rootView.findViewById(R.id.section_label);
-            textView.setText(getString(R.string.section_format, getArguments().getInt(ARG_SECTION_NUMBER)));
-            return rootView;
-        }
-    }
-
-    /**
-     * A {@link FragmentPagerAdapter} that returns a fragment corresponding to
-     * one of the sections/tabs/pages.
-     */
-    public class SectionsPagerAdapter extends FragmentPagerAdapter {
-
-        public SectionsPagerAdapter(FragmentManager fm) {
-            super(fm);
-        }
-
-        @Override
-        public Fragment getItem(int position) {
-            // getItem is called to instantiate the fragment for the given page.
-            // Return a PlaceholderFragment (defined as a static inner class below).
-            return PlaceholderFragment.newInstance(position + 1);
-        }
-
-        @Override
-        public int getCount() {
-            // Show 3 total pages.
-            return 3;
-        }
-
-        @Override
-        public CharSequence getPageTitle(int position) {
-            switch (position) {
-                case 0:
-                    return "SECTION 1";
-                case 1:
-                    return "SECTION 2";
-                case 2:
-                    return "SECTION 3";
             }
-            return null;
+
+            @Override
+            public void onPageSelected(int position) {
+                createDots(position);
+                if(position==layouts.length-1){
+                    btnFinish.setVisibility(View.VISIBLE);
+                    btnNext.setVisibility(View.INVISIBLE);
+                    btnSkip.setVisibility(View.INVISIBLE);
+                }
+                else{
+                    btnFinish.setVisibility(View.INVISIBLE);
+                    btnNext.setVisibility(View.VISIBLE);
+                }
+            }
+
+            @Override
+            public void onPageScrollStateChanged(int state) {
+
+            }
+        });
+
+        btnNext = findViewById(R.id.btnNext);
+        btnSkip = findViewById(R.id.btnSkip);
+        btnFinish = findViewById(R.id.btnFinish);
+        btnNext.setOnClickListener(this);
+        btnSkip.setOnClickListener(this);
+        btnFinish.setOnClickListener(this);
+
+    }
+
+    private void createDots(int current_position){
+        if(dots_layout != null){
+            dots_layout.removeAllViews();
+        }
+
+        dots = new ImageView[layouts.length];
+
+        for(int i = 0; i<layouts.length; i++){
+            dots[i] = new ImageView(this);
+            if(i == current_position)
+            {
+                dots[i].setImageDrawable(ContextCompat.getDrawable(this, R.drawable.active_dot));
+            }
+            else
+            {
+                dots[i].setImageDrawable(ContextCompat.getDrawable(this, R.drawable.inactive_dot));
+            }
+
+            LinearLayout.LayoutParams param = new LinearLayout.LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT);
+            param.setMargins(6,0,6,0);
+            dots_layout.addView(dots[i], param);
+        }
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch(v.getId()){
+            case R.id.btnNext:
+                loadNextSlide();
+                break;
+
+            case R.id.btnSkip:
+                loadHome();
+                break;
+            case R.id.btnFinish:
+                loadHome();
+                break;
+        }
+    }
+
+    private void loadHome()
+    {
+        finish();
+    }
+
+    private void loadNextSlide()
+    {
+        try
+        {
+            int next_slide = mViewPager.getCurrentItem()+1;
+            if(next_slide<layouts.length)
+            {
+                mViewPager.setCurrentItem(next_slide);
+            }else {
+                finish();
+            }
+        }
+        catch(Exception e)
+        {
+            AlertDialog.Builder a = new AlertDialog.Builder(this);
+            a.setMessage(String.valueOf(e.getMessage()));
+            a.show();
         }
     }
 }
