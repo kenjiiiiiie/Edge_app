f22dd7c2d7e39060ef3cf2dc90a011bfa27f60b6
CheesyBeer <44321294+cnmberches@users.noreply.github.com>
2019-03-12 15:48:08 +0800

    On boarding activity

--- app/src/main/java/app/helianthus/edge/MainActivity.java
+++ app/src/main/java/app/helianthus/edge/MainActivity.java
@@ -1,6 +1,7 @@
 package app.helianthus.edge;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
@@ -13,6 +14,7 @@ import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
 
+import android.preference.PreferenceManager;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
@@ -32,6 +34,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(getBaseContext());
+        boolean previouslyStarted = prefs.getBoolean(getString(R.string.first_launch), false);
+        if (!previouslyStarted) {
+            SharedPreferences.Editor edit = prefs.edit();
+            edit.putBoolean(getString(R.string.first_launch), Boolean.TRUE).apply();
+            MainActivity.this.startActivity(new Intent(MainActivity.this, ActivityOnboarding.class));
+        }
 
         fragmentManager.beginTransaction().add(R.id.main_fragment_container, fragmentHome, "1").commit();
 
