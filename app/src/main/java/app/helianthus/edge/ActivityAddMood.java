e108dd9026021f980b481269e9e1f1697a67a521
Kenneth Nuñezca <manilaboy07@gmail.com>
2019-03-15 21:37:54 +0800

    Stress

--- app/src/main/java/app/helianthus/edge/ActivityAddMood.java
+++ app/src/main/java/app/helianthus/edge/ActivityAddMood.java
@@ -1,8 +1,11 @@
 package app.helianthus.edge;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
+import android.os.Build;
 import android.os.Bundle;
+import android.view.View;
 
 public class ActivityAddMood extends AppCompatActivity {
 
@@ -10,5 +13,17 @@ public class ActivityAddMood extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_mood);
+
+        View view = findViewById(R.id.add_mood_root);
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            view.setSystemUiVisibility(View.SYSTEM_UI_FLAG_LIGHT_NAVIGATION_BAR);
+        }
+
+        Toolbar toolbar = findViewById(R.id.add_mood_toolbar);
+        setSupportActionBar(toolbar);
+        toolbar.inflateMenu(R.menu.journal_write_app_bar_menu);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setDisplayShowTitleEnabled(false);
+        getSupportActionBar().setHomeAsUpIndicator(R.drawable.ic_cancel);
     }
 }
--- app/src/main/java/app/helianthus/edge/ActivityStress.java
+++ app/src/main/java/app/helianthus/edge/ActivityStress.java
@@ -8,13 +8,23 @@ import android.view.View;
 import android.view.Window;
 
 import com.google.android.material.appbar.CollapsingToolbarLayout;
+
+import java.util.ArrayList;
+
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 import androidx.palette.graphics.Palette;
+import androidx.recyclerview.widget.DividerItemDecoration;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 public class ActivityStress extends AppCompatActivity {
 
     CollapsingToolbarLayout collapsingToolbarLayout;
+
+    RecyclerView recyclerView;
+    private ListAdapter adapter;
+    private ListData[] listData;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -44,6 +54,18 @@ public class ActivityStress extends AppCompatActivity {
             collapsingToolbarLayout.setContentScrimColor(extractedPalette.getRgb());
             window.setStatusBarColor(extractedPalette.getRgb());
         }
+
+        listData = new ListData[] {
+                new ListData("1", getString(R.string.stress_title_01)),
+                new ListData("2", getString(R.string.stress_title_02)),
+        };
+
+        recyclerView = findViewById(R.id.reads_stress_recycler_view);
+        LinearLayoutManager linearLayoutManager = new LinearLayoutManager(this);
+        adapter = new ListAdapter(listData);
+        recyclerView.setHasFixedSize(true);
+        recyclerView.setLayoutManager(linearLayoutManager);
+        recyclerView.setAdapter(adapter);
 
     }
 
