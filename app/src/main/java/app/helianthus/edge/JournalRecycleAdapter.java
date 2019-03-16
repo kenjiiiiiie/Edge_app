5baf7158dfaffca4c7d4a987255a3342a89b2dde
Kenneth Nuñezca <manilaboy07@gmail.com>
2019-03-16 22:42:39 +0800

    Stress

--- app/src/main/java/app/helianthus/edge/JournalRecycleAdapter.java
+++ app/src/main/java/app/helianthus/edge/JournalRecycleAdapter.java
@@ -1,20 +1,15 @@
 package app.helianthus.edge;
 
-import android.content.Context;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.FrameLayout;
 import android.widget.LinearLayout;
 import android.widget.TextView;
-
-import com.google.android.material.card.MaterialCardView;
 
 import java.util.ArrayList;
 import java.util.Objects;
 
 import androidx.annotation.NonNull;
-import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.RecyclerView;
 
 public class JournalRecycleAdapter extends RecyclerView.Adapter<JournalRecycleAdapter.ViewHolder> {
