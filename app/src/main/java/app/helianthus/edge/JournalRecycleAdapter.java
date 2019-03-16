57f2341d2310eb02281ffb510a0b6463e9c5bbf0
CheesyBeer <44321294+cnmberches@users.noreply.github.com>
2019-03-16 22:29:27 +0800

    Update On Journal

---
+++ app/src/main/java/app/helianthus/edge/JournalRecycleAdapter.java
@@ -0,0 +1,69 @@
+package app.helianthus.edge;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.FrameLayout;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import com.google.android.material.card.MaterialCardView;
+
+import java.util.ArrayList;
+import java.util.Objects;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.RecyclerView;
+
+public class JournalRecycleAdapter extends RecyclerView.Adapter<JournalRecycleAdapter.ViewHolder> {
+    private ArrayList<String> dateText, contentText, preview;
+
+    static class ViewHolder extends RecyclerView.ViewHolder {
+        LinearLayout cardView;
+        TextView cardTextView, dateTextView;
+
+        ViewHolder(@NonNull View itemView) {
+            super(itemView);
+            cardView = itemView.findViewById(R.id.journal_card);
+            cardTextView = itemView.findViewById(R.id.journal_card_content);
+            dateTextView =  itemView.findViewById(R.id.journal_card_date);
+        }
+    }
+
+    JournalRecycleAdapter(ArrayList<String> dateTextView, ArrayList<String> contentTextView, ArrayList<String> preview) {
+        this.dateText = dateTextView;
+        this.contentText = contentTextView;
+        this.preview = preview;
+    }
+
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        LayoutInflater inflater =  LayoutInflater.from(parent.getContext());
+
+        View materialCard = inflater.inflate(R.layout.journal_card, parent, false);
+        return new ViewHolder(materialCard);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder holder, final int position) {
+        Objects.requireNonNull(holder).cardTextView.setText(preview.get(position));
+        Objects.requireNonNull(holder).dateTextView.setText(dateText.get(position));
+        holder.cardView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                ActivityWriteJournal.write_isEdit = true;
+                ActivityWriteJournal.write_content = contentText.get(position);
+                FragmentJournal.startWriteJournal_Activity();
+            }
+        });
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return dateText.size();
+    }
+}
